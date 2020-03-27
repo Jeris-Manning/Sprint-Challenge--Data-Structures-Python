ac27e14171595c5019917384d2453e7b8f550b02
Jeris Manning <the.goose.moose@gmail.com>
2020-03-27 13:02:34 -0600

    noted runtime in names problem

--- names/names.py
+++ names/names.py
@@ -8,20 +8,18 @@ sys.setrecursionlimit(10**6)
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
-# print(names_1)
+
+
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_3.txt', 'r')
-names_3 = f.read().split("\n")  # List containing 10000 names
-f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
 # for name_1 in names_1:
-#     for name_2 in names_2:
+#     for name_2 in names_2:     # This is runtime O(n^2)
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
